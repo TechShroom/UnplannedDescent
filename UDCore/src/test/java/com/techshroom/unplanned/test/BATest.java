3e1403c8e077b6dc782ca553751762db634b534d
Kenzie Togami <ket1999@gmail.com>
2014-06-26 20:24:15 -0700

    More testing stuffs

--- UDCore/src/test/java/com/techshroom/unplanned/test/BATest.java
+++ UDCore/src/test/java/com/techshroom/unplanned/test/BATest.java
@@ -2,23 +2,34 @@ package com.techshroom.unplanned.test;
 
 import static org.junit.Assert.assertArrayEquals;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.techshroom.unplanned.modloader.BetterArrays;
 
 public class BATest {
 
-	private Object[] array, reverse;
-	private int[] arrayi, reversei;
+	private static Object[] GENERIC_array, GENERIC_reverse;
+	private static int[] NOTGENERIC_arrayi, NOTGENERIC_reversei;
+	private static Object[] FILL_notfilled, FILL_filled;
+	private static Object[] SPL_original, SPL_normalcp, SPL_reversecp,
+			SPL_bigstepcp, SPL_rbigstepcp;
 
-	@Before
-	public void setUp() throws Exception {
+	@BeforeClass
+	public static void setUp() throws Exception {
 		Object obj = new Object();
-		array = new Object[] { "abc", 1, 2, 3, obj };
-		reverse = new Object[] { obj, 3, 2, 1, "abc" };
-		arrayi = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-		reversei = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+		GENERIC_array = new Object[] { "abc", 1, 2, 3, obj };
+		GENERIC_reverse = new Object[] { obj, 3, 2, 1, "abc" };
+		NOTGENERIC_arrayi = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		NOTGENERIC_reversei = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+		FILL_notfilled = new Object[10];
+		FILL_filled = new Object[] { 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a',
+				'a', 'a' };
+		SPL_original = new Object[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		SPL_normalcp = new Object[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		SPL_reversecp = new Object[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+		SPL_bigstepcp = new Object[] { 0, 2, 4, 6, 8 };
+		SPL_rbigstepcp = new Object[] { 9, 7, 5, 3, 1 };
 	}
 
 	/**
@@ -26,8 +37,8 @@ public class BATest {
 	 */
 	@Test
 	public void reversesArray() {
-		Object[] reversed_ba = BetterArrays.reverse(array);
-		assertArrayEquals(reverse, reversed_ba);
+		Object[] reversed_ba = BetterArrays.reverse(GENERIC_array);
+		assertArrayEquals(GENERIC_reverse, reversed_ba);
 	}
 
 	/**
@@ -35,7 +46,50 @@ public class BATest {
 	 */
 	@Test
 	public void reversesNonGenericArray() {
-		int[] reversed_ba = BetterArrays.reverseNonGeneric(arrayi);
-		assertArrayEquals(reversei, reversed_ba);
+		int[] reversed_ba = BetterArrays.reverseNonGeneric(NOTGENERIC_arrayi);
+		assertArrayEquals(NOTGENERIC_reversei, reversed_ba);
+	}
+
+	/**
+	 * BetterArrays can fill an array.
+	 */
+	@Test
+	public void fillsArray() {
+		BetterArrays.fillArray(FILL_notfilled, 'a');
+		assertArrayEquals(FILL_filled, FILL_notfilled);
+	}
+
+	/**
+	 * BetterArrays can create and fill an array.
+	 */
+	@Test
+	public void createAndFillsArray() {
+		Object[] wascreated = BetterArrays.createAndFill(Object.class, 10, 'a');
+		assertArrayEquals(FILL_filled, wascreated);
+	}
+
+	/**
+	 * BetterArrays can splice an array.
+	 */
+	@Test
+	public void splicesArray() {
+		// check that no in place mods occur
+		Object[] test, defensiveCopy = SPL_original.clone();
+		// splice no args: copy
+		test = BetterArrays.splice(SPL_original);
+		assertArrayEquals(defensiveCopy, SPL_original);
+		assertArrayEquals(SPL_normalcp, test);
+		// splice 0, -1, -1: reverse copy
+		test = BetterArrays.splice(SPL_original, 0, -1, -1);
+		assertArrayEquals(defensiveCopy, SPL_original);
+		assertArrayEquals(SPL_reversecp, test);
+		// splice 0, -1, 2: every other object
+		test = BetterArrays.splice(SPL_original, 0, -1, 2);
+		assertArrayEquals(defensiveCopy, SPL_original);
+		assertArrayEquals(SPL_bigstepcp, test);
+		// splice 0, -1, -2: reverse every other object
+		test = BetterArrays.splice(SPL_original, 0, -1, -2);
+		assertArrayEquals(defensiveCopy, SPL_original);
+		assertArrayEquals(SPL_rbigstepcp, test);
 	}
 }
