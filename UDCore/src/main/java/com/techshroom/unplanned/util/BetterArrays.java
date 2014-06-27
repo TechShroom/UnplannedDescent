ee175085dc6843c62e9855b20534716cfb64531b
Kenzie Togami <ket1999@gmail.com>
2014-06-27 15:22:28 -0700

    Move some array based stuff

--- UDCore/src/main/java/com/techshroom/unplanned/util/BetterArrays.java
+++ UDCore/src/main/java/com/techshroom/unplanned/util/BetterArrays.java
@@ -6,201 +6,299 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public final class BetterArrays {
-    public static void print(Object[] array) {
-        print(array, System.err);
-    }
+	public static void print(Object[] array) {
+		print(array, System.err);
+	}
 
-    public static void print(Object[] array, PrintStream o) {
-        if (array == null) {
-            o.println("null");
-        }
-        o.println(array.getClass().getComponentType().getName()
-                + "[] (length: " + array.length + ") contents:");
-        int index = 0;
-        for (Object object : array) {
-            String out = "";
-            String indexs = index + ": ";
-            String clazs = "";
-            if (object == null) {
-                out = "<null object>";
-                clazs = array.getClass().getComponentType().getName();
-            } else if (object instanceof String && ((String) object).equals("")) {
-                out = "<empty string>";
-                clazs = object.getClass().getName();
-            } else {
-                out = object.toString();
-                clazs = object.getClass().getName();
-            }
-            o.println(indexs + "(" + clazs + ") " + out);
-            index++;
-        }
-    }
+	public static void print(Object[] array, PrintStream o) {
+		if (array == null) {
+			o.println("null");
+		}
+		o.println(array.getClass().getComponentType().getName()
+				+ "[] (length: " + array.length + ") contents:");
+		int index = 0;
+		for (Object object : array) {
+			String out = "";
+			String indexs = index + ": ";
+			String clazs = "";
+			if (object == null) {
+				out = "<null object>";
+				clazs = array.getClass().getComponentType().getName();
+			} else if (object instanceof String && ((String) object).equals("")) {
+				out = "<empty string>";
+				clazs = object.getClass().getName();
+			} else {
+				out = object.toString();
+				clazs = object.getClass().getName();
+			}
+			o.println(indexs + "(" + clazs + ") " + out);
+			index++;
+		}
+	}
 
-    public static <T, D> T[] arrayTranslate(Class<T> generic, D[] src) {
-        ArrayList<T> temp = new ArrayList<T>(src.length);
-        for (D o : src) {
-            if (o != null && generic.isAssignableFrom(o.getClass())) {
-                temp.add(generic.cast(o));
-            } else if (o == null) {
-                temp.add((T) null);
-            } else {
-                System.err.println("Lost " + o + " because it was not of type "
-                        + generic.getName());
-            }
-        }
-        T[] arrType = newArray(generic, src.length);
-        return temp.toArray(arrType);
-    }
+	public static <T, D> T[] arrayTranslate(Class<T> generic, D[] src) {
+		ArrayList<T> temp = new ArrayList<T>(src.length);
+		for (D o : src) {
+			if (o != null && generic.isAssignableFrom(o.getClass())) {
+				temp.add(generic.cast(o));
+			} else if (o == null) {
+				temp.add((T) null);
+			} else {
+				System.err.println("Lost " + o + " because it was not of type "
+						+ generic.getName());
+			}
+		}
+		T[] arrType = newArray(generic, src.length);
+		return temp.toArray(arrType);
+	}
 
-    public static <T> T[] randomArray(T[] in) {
-        if (in.length == 0 || in.length == 1) {
-            return in;
-        }
-        T[] test = newArray(in.getClass().getComponentType(), in.length);
-        boolean solved = false;
-        boolean[] taken = new boolean[test.length];
-        int total = test.length;
-        Random r = new Random(new Random().nextInt());
-        int index = 0;
-        while (!solved) {
-            int ra = r.nextInt(test.length);
-            if (!taken[ra]) {
-                test[ra] = in[index];
-                taken[ra] = true;
-                index++;
-                total--;
-            }
-            if (total == 0) {
-                solved = true;
-            }
-        }
-        return test;
-    }
+	public static <T> T[] randomArray(T[] in) {
+		if (in.length == 0 || in.length == 1) {
+			return in;
+		}
+		T[] test = newArray(in.getClass().getComponentType(), in.length);
+		boolean solved = false;
+		boolean[] taken = new boolean[test.length];
+		int total = test.length;
+		Random r = new Random(new Random().nextInt());
+		int index = 0;
+		while (!solved) {
+			int ra = r.nextInt(test.length);
+			if (!taken[ra]) {
+				test[ra] = in[index];
+				taken[ra] = true;
+				index++;
+				total--;
+			}
+			if (total == 0) {
+				solved = true;
+			}
+		}
+		return test;
+	}
 
-    public static void dump(Object[] array) {
-        for (Object t : array) {
-            System.out.println(t);
-        }
-    }
+	public static void dump(Object[] array) {
+		for (Object t : array) {
+			System.out.println(t);
+		}
+	}
 
-    public static <T> T[] repeatRandomArray(T[] in, int count) {
-        T[] array = newArray(in.getClass().getComponentType(), in.length);
-        System.arraycopy(in, 0, array, 0, in.length);
-        while (count > -1) {
-            array = BetterArrays.randomArray(array);
-            count--;
-        }
-        return array;
-    }
+	public static <T> T[] repeatRandomArray(T[] in, int count) {
+		T[] array = newArray(in.getClass().getComponentType(), in.length);
+		System.arraycopy(in, 0, array, 0, in.length);
+		while (count > -1) {
+			array = BetterArrays.randomArray(array);
+			count--;
+		}
+		return array;
+	}
 
-    public static String dump0(Object[] array) {
-        if (array == null) {
-            return "<null array>";
-        }
-        if (array.length == 0) {
-            return "[]";
-        }
-        String ret = "[";
-        for (Object o : array) {
-            if (o instanceof String) {
-                o = "'" + o + "'";
-            }
-            ret += o + ", ";
-        }
-        ret = ret.substring(0, ret.length() - 2) + "]";
-        return ret;
-    }
+	public static String dump0(Object[] array) {
+		if (array == null) {
+			return "<null array>";
+		}
+		if (array.length == 0) {
+			return "[]";
+		}
+		String ret = "[";
+		for (Object o : array) {
+			if (o instanceof String) {
+				o = "'" + o + "'";
+			}
+			ret += o + ", ";
+		}
+		ret = ret.substring(0, ret.length() - 2) + "]";
+		return ret;
+	}
 
-    public static byte[] intToByteArray(int value) {
-        return new byte[] { (byte) (value >>> 24), (byte) (value >>> 16),
-                (byte) (value >>> 8), (byte) value };
-    }
+	public static byte[] intToByteArray(int value) {
+		return new byte[] { (byte) (value >>> 24), (byte) (value >>> 16),
+				(byte) (value >>> 8), (byte) value };
+	}
 
-    public static <T> T[] reverse(T[] stuff) {
-        T[] out = newArray(stuff.getClass().getComponentType(), stuff.length);
-        for (int i = 0; i < stuff.length; i++) {
-            out[stuff.length - i - 1] = stuff[i];
-        }
-        return out;
-    }
+	public static <T> T[] reverse(T[] stuff) {
+		T[] out = newArray(stuff.getClass().getComponentType(), stuff.length);
+		for (int i = 0; i < stuff.length; i++) {
+			out[stuff.length - i - 1] = stuff[i];
+		}
+		return out;
+	}
 
-    /*
-     * This is a cool trick which allows us to return the requested array even
-     * with primitives as the component type.
-     */
-    public static <T> T reverseNonGeneric(T t) {
-        int tlen = Array.getLength(t);
-        T out = newArray(t.getClass().getComponentType(), tlen);
-        for (int i = 0; i < tlen; i++) {
-            Array.set(out, tlen - i - 1, Array.get(t, i));
-        }
-        return out;
-    }
+	/*
+	 * This is a cool trick which allows us to return the requested array even
+	 * with primitives as the component type.
+	 */
+	public static <T> T reverseNonGeneric(T t) {
+		int tlen = Array.getLength(t);
+		T out = newArray(t.getClass().getComponentType(), tlen);
+		for (int i = 0; i < tlen; i++) {
+			Array.set(out, tlen - i - 1, Array.get(t, i));
+		}
+		return out;
+	}
 
-    public static void fillArray(Object array, Object value) {
-        for (int i = 0; i < Array.getLength(array); i++) {
-            Array.set(array, i, value);
-        }
-    }
+	public static void fillArray(Object array, Object value) {
+		for (int i = 0; i < Array.getLength(array); i++) {
+			Array.set(array, i, value);
+		}
+	}
 
-    public static <T> T createAndFill(Class<?> type, int size, Object value) {
-        T array = newArray(type, size);
-        fillArray(array, value);
-        return array;
-    }
+	public static <T> T createAndFill(Class<?> type, int size, Object value) {
+		T array = newArray(type, size);
+		fillArray(array, value);
+		return array;
+	}
 
-    public static <T> T splice(T array, int... splice) {
-        int slen = splice.length;
-        int start = 0, end = Array.getLength(array), step = 1;
+	public static <T> T splice(T array, int... splice) {
+		int slen = splice.length;
+		int start = 0, end = Array.getLength(array), step = 1;
 
-        if (slen > 0) {
-            start = splice[0];
-        }
-        if (slen > 1) {
-            end = splice[1];
-            if (end < 0) {
-            	end = Array.getLength(array);
-            }
-        }
-        if (slen > 2) {
-            step = splice[2];
-        }
+		if (slen > 0) {
+			start = splice[0];
+		}
+		if (slen > 1) {
+			end = splice[1];
+			if (end < 0) {
+				end = Array.getLength(array);
+			}
+		}
+		if (slen > 2) {
+			step = splice[2];
+		}
 
-        if (start < 0) {
-            throw new IndexOutOfBoundsException("start < 0");
-        }
-        if (end > Array.getLength(array)) {
-            throw new IndexOutOfBoundsException("end > length");
-        }
-        if (end < start) {
-            throw new IllegalArgumentException("start < end");
-        }
-        if (step == 0) {
-            throw new IllegalArgumentException("step == 0");
-        }
-        int len = end - start; // get length when step == 1
-        boolean back = step < 0;
-        step = Math.abs(step);
-        if (step > 1) {
-            int mod = len % step; // modulo to get leftovers
-            len -= mod; // remove them to floor the result
-            len /= step; // divide by step
-        }
-        T out = newArray(array.getClass().getComponentType(), len);
-        if (back) {
-            for (int i = end - 1, index = 0; i >= start; i -= step, index++) {
-                Array.set(out, index, Array.get(array, i));
-            }
-        } else {
-            for (int i = start, index = 0; i < end; i += step, index++) {
-                Array.set(out, index, Array.get(array, i));
-            }
-        }
-        return out;
-    }
+		if (start < 0) {
+			throw new IndexOutOfBoundsException("start < 0");
+		}
+		if (end > Array.getLength(array)) {
+			throw new IndexOutOfBoundsException("end > length");
+		}
+		if (end < start) {
+			throw new IllegalArgumentException("start < end");
+		}
+		if (step == 0) {
+			throw new IllegalArgumentException("step == 0");
+		}
+		int len = end - start; // get length when step == 1
+		boolean back = step < 0;
+		step = Math.abs(step);
+		if (step > 1) {
+			int mod = len % step; // modulo to get leftovers
+			len -= mod; // remove them to floor the result
+			len /= step; // divide by step
+		}
+		T out = newArray(array.getClass().getComponentType(), len);
+		if (back) {
+			for (int i = end - 1, index = 0; i >= start; i -= step, index++) {
+				Array.set(out, index, Array.get(array, i));
+			}
+		} else {
+			for (int i = start, index = 0; i < end; i += step, index++) {
+				Array.set(out, index, Array.get(array, i));
+			}
+		}
+		return out;
+	}
 
-    @SuppressWarnings("unchecked")
-    public static <T> T newArray(Class<?> component, int len) {
-        return (T) Array.newInstance(component, len);
-    }
+	@SuppressWarnings("unchecked")
+	public static <T> T newArray(Class<?> component, int len) {
+		return (T) Array.newInstance(component, len);
+	}
+
+	/**
+	 * Gets a boolean argument safely
+	 * 
+	 * @param args
+	 *            - the args list from which to retrieve the argument
+	 * @param index
+	 *            - the index of the wanted argument
+	 * @param def
+	 *            - a default value to fallback on
+	 * @return the wanted boolean argument value, or the default value
+	 */
+	public static boolean getArgB(String[] args, int index, boolean def) {
+		return Boolean.valueOf(getArgS(args, index, Boolean.valueOf(def)
+				.toString()));
+	}
+
+	/**
+	 * Gets a integer argument safely
+	 * 
+	 * @param args
+	 *            - the args list from which to retrieve the argument
+	 * @param index
+	 *            - the index of the wanted argument
+	 * @param def
+	 *            - a default value to fallback on
+	 * @return the wanted integer argument value, or the default value
+	 */
+	public static int getArgI(String[] args, int index, int def) {
+		return Integer.valueOf(getArgS(args, index, Integer.valueOf(def)
+				.toString()));
+	}
+
+	/**
+	 * Gets a float argument safely
+	 * 
+	 * @param args
+	 *            - the args list from which to retrieve the argument
+	 * @param index
+	 *            - the index of the wanted argument
+	 * @param def
+	 *            - a default value to fallback on
+	 * @return the wanted float argument value, or the default value
+	 */
+	public static float getArgF(String[] args, int index, float def) {
+		return Float
+				.valueOf(getArgS(args, index, Float.valueOf(def).toString()));
+	}
+
+	/**
+	 * Gets a double argument safely
+	 * 
+	 * @param args
+	 *            - the args list from which to retrieve the argument
+	 * @param index
+	 *            - the index of the wanted argument
+	 * @param def
+	 *            - a default value to fallback on
+	 * @return the wanted double argument value, or the default value
+	 */
+	public static double getArgD(String[] args, int index, double def) {
+		return Double.valueOf(getArgS(args, index, Double.valueOf(def)
+				.toString()));
+	}
+
+	/**
+	 * Gets a String argument safely
+	 * 
+	 * @param args
+	 *            - the args list from which to retrieve the argument
+	 * @param index
+	 *            - the index of the wanted argument
+	 * @param def
+	 *            - a default value to fallback on
+	 * @return the wanted String argument value, or the default value
+	 */
+	public static String getArgS(String[] args, int index, String def) {
+		return getArg(args, index, def);
+	}
+
+	/**
+	 * Gets an argument safely
+	 * 
+	 * @param args
+	 *            - the args list from which to retrieve the argument
+	 * @param index
+	 *            - the index of the wanted argument
+	 * @param def
+	 *            - a default value to fallback on
+	 * @return the wanted argument value, or the default value
+	 */
+	public static <T> T getArg(T[] args, int index, T def) {
+		if (args == null) {
+			return def;
+		}
+		return args.length <= index ? def : args[index] == null ? def
+				: args[index];
+	}
 }
