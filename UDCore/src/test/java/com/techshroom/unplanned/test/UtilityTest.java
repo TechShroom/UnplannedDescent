b59402ffdbe1ae36a768b97d5d9e00179d914332
Kenzie Togami <ket1999@gmail.com>
2014-06-27 18:57:46 -0700

    LUtils test started
    [ci skip]

---
+++ UDCore/src/test/java/com/techshroom/unplanned/test/UtilityTest.java
@@ -0,0 +1,14 @@
+package com.techshroom.unplanned.test;
+
+import org.junit.Test;
+
+public class UtilityTest {
+
+	/**
+	 * LUtils can limit the printing to certain groups.
+	 */
+	@Test
+	public void limitsGroups() {
+	}
+
+}
