2d21b767f1f5e957e145a44f0be74c9c825eba46
Kenzie Togami <ket1999@gmail.com>
2015-08-11 20:39:29 -0700

    Add pretty join functionality

--- UDAPI/src/main/java/com/techshroom/unplanned/core/util/Strings.java
+++ UDAPI/src/main/java/com/techshroom/unplanned/core/util/Strings.java
@@ -1,6 +1,11 @@
 package com.techshroom.unplanned.core.util;
 
-public class Strings {
+import java.util.Iterator;
+import java.util.Objects;
+
+import com.google.common.collect.Iterators;
+
+public final class Strings {
 
     /**
      * Returns the amount of <tt>c</tt>'s in <tt>s</tt>
@@ -51,4 +56,85 @@ public class Strings {
         }
         return (byte) (c - '0');
     }
+
+    /**
+     * Joins {@code items} by {@code join} if there is more than two, prefixing
+     * the last item with {@code " " + lastPrefix} if there is more than one,
+     * and prefixing every item but the first with {@code " "}.
+     * <p>
+     * This is useful for generating lists that are viewed by users by setting
+     * {@code lastPrefix} to "and" or "or":
+     * 
+     * <pre>
+     * Iterable<String> items = Stream.of("item1")::iterator;
+     * Strings.prettyJoin(",", "and", items) -> "item1"
+     * items = Stream.of("item1", "item2")::iterator;
+     * Strings.prettyJoin(",", "and", items) -> "item1 and item2"
+     * items = Stream.of("item1", "item2", "item3")::iterator;
+     * Strings.prettyJoin(",", "and", items) -> "item1, item2, and item3"
+     * </pre>
+     * 
+     * These 2 cases are common enough for overrides to exist,
+     * {@link #prettyJoinAnd} and {@link #prettyJoinOr}.
+     * </p>
+     * 
+     * @param join
+     * @param lastPrefix
+     * @param items
+     * @return
+     */
+    public static <X> String prettyJoin(String join, String lastPrefix,
+            Iterable<X> items) {
+        Iterator<? extends CharSequence> iter =
+                Iterators.transform(items.iterator(), Objects::toString);
+        if (!iter.hasNext()) {
+            // no items quick-return
+            return "";
+        }
+        // start with item1
+        StringBuilder b = new StringBuilder(iter.next());
+        if (iter.hasNext()) {
+            iter = Iterators.transform(iter, e -> " " + e);
+            // 2+ items mode, use lastPrefix
+            CharSequence item2 = iter.next();
+            if (iter.hasNext()) {
+                // 3+ items mode, use join
+                b.append(join).append(item2);
+                while (iter.hasNext()) {
+                    CharSequence next = iter.next();
+                    b.append(join);
+                    if (!iter.hasNext()) {
+                        b.append(" ").append(lastPrefix);
+                    }
+                    b.append(next);
+                }
+            } else {
+                // 2 items
+                b.append(" ").append(lastPrefix).append(item2);
+            }
+        }
+        return b.toString();
+    }
+
+    public static <X> String prettyJoinAnd(Iterable<X> items) {
+        return prettyJoin(",", "and", items);
+    }
+
+    public static <X> String prettyJoinOr(Iterable<X> items) {
+        return prettyJoin(",", "or", items);
+    }
+
+    public static String prettyJoin(String join, String lastPrefix,
+            Object... items) {
+        return prettyJoin(join, lastPrefix, () -> Iterators.forArray(items));
+    }
+
+    public static String prettyJoinAnd(Object... items) {
+        return prettyJoinAnd(() -> Iterators.forArray(items));
+    }
+
+    public static String prettyJoinOr(Object... items) {
+        return prettyJoinOr(() -> Iterators.forArray(items));
+    }
+
 }
