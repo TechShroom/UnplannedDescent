e8c5bbfe1e8bcf0da62a1fb9c9ab83c5c1fd348d
Kenzie Togami <ket1999@gmail.com>
2015-11-25 11:59:00 -0800

    Add some checks for ModMetadata

--- UDAPI/src/main/java/com/techshroom/unplanned/core/mod/ModMetadata.java
+++ UDAPI/src/main/java/com/techshroom/unplanned/core/mod/ModMetadata.java
@@ -1,6 +1,9 @@
 package com.techshroom.unplanned.core.mod;
 
+import static com.google.common.base.Preconditions.checkState;
+
 import com.google.auto.value.AutoValue;
+import com.techshroom.unplanned.core.util.UDStrings;
 
 /**
  * Data about a mod.
@@ -13,23 +16,40 @@ public abstract class ModMetadata {
     }
 
     @AutoValue.Builder
-    public interface Builder {
+    public static abstract class Builder {
 
-        default Builder idAndName(String id) {
+        Builder() {
+        }
+
+        public Builder idAndName(String id) {
             return id(id).name(id);
         }
 
-        Builder id(String id);
+        public abstract Builder id(String id);
 
-        Builder name(String name);
+        public abstract Builder name(String name);
 
-        Builder version(String version);
+        public abstract Builder version(String version);
 
-        Builder buildNumber(long buildNumber);
+        public abstract Builder buildNumber(long buildNumber);
 
-        Builder targetGameVersion(String targetGameVersion);
+        public abstract Builder targetGameVersion(String targetGameVersion);
 
-        ModMetadata build();
+        abstract ModMetadata autoBuild();
+
+        public ModMetadata build() {
+            ModMetadata built = autoBuild();
+            // TODO semver checks
+            checkState(UDStrings.count(built.getVersion(), '.') >= 2,
+                    "version must be semver");
+            checkState(!built.getId().isEmpty(), "ID cannot be empty");
+            checkState(!built.getName().isEmpty(), "Name cannot be empty");
+            checkState(!built.getTargetGameVersion().isEmpty(),
+                    "Target game version cannot be empty");
+            checkState(built.getBuildNumber() >= 0,
+                    "Build number must be positive");
+            return built;
+        }
 
     }
 
@@ -37,47 +57,49 @@ public abstract class ModMetadata {
     }
 
     /**
-     * Returns the id of the mod. This id <b>must</b> be unique, if there are
-     * non-unique ids there will be a crash!
+     * Returns the id of the mod, which cannot be empty. This id <b>must</b> be
+     * unique, if there are non-unique ids there will be a crash!
      * 
      * @return the id of the mod
      */
     public abstract String getId();
 
     /**
-     * Returns the display name of the mod. This may be non-unique.
+     * Returns the display name of the mod, which cannot be empty. This may be
+     * non-unique.
      * 
      * @return the id of the mod
      */
     public abstract String getName();
 
     /**
-     * Returns the version of the mod. This version may be used on multiple
-     * builds of a mod.
+     * Returns the version of the mod, which MUST be in semver style. This
+     * version may be used on multiple builds of a mod.
      * 
      * @return the version of the mod
      */
     public abstract String getVersion();
 
     /**
-     * Returns the build number of the mod. This must be incremented every time
-     * there is a mod upgrade, any changes to the code must increase this
-     * number.
+     * Returns the build number of the mod, which must be positive. This must be
+     * incremented every time there is a mod upgrade, any changes to the code
+     * must increase this number.
      * 
      * @return the build number of the mod
      */
     public abstract long getBuildNumber();
 
     /**
-     * Returns the targeted game version of the mod. A game may choose to load
-     * the mod in non-matching versions if the API is non-breaking.
+     * Returns the targeted game version of the mod, which may not be empty. A
+     * game may choose to load the mod in non-matching versions if the API is
+     * non-breaking.
      * 
      * @return the targeted game version of the mod
      */
     public abstract String getTargetGameVersion();
 
     @Override
-    public String toString() {
+    public final String toString() {
         return String.format(
                 "%s[name=%s,version=%s,buildNumber=%s,targetGameVersion=%s]",
                 getId(), getName(), getVersion(), getBuildNumber(),
